7bab3158cb47214d5883cf43dce29a864c06b5d9
Jack Li <jl15060@nyu.edu>
2024-10-25 13:51:59 -0400

    Initial commit of array multiplier for tiny tapeout

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_b_12_array_multiplier (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,81 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  
+wire [3:0] m = ui_in[7:4];
+wire [3:0] q = ui_in[3:0];
+wire [7:0] p;
+
+
+
+
+
+	input [3:0] m,
+	input [3:0] q,
+	output [7:0] p
+);
+    wire [2:0] o1;
+    wire [2:0] o2;
+    wire [2:0] o3;
+     wire [2:0] o4;
+
+	wire [3:0] c;
+	
+    assign p[4]=o4[0];
+    assign p[5]=o4[1];
+    assign p[6]=o4[2];
+    assign p[7]=c[3];
+	part pa (m,3'b000,0,q[0],o1,c[0],p[0]);
+	part pb (m,o1,c[0],q[1],o2,c[1],p[1]);
+	part pc (m,o2,c[1],q[2],o3,c[2],p[2]);
+	part pd (m,o3,c[2],q[3],o4,c[3],p[3]);
+  
+  assign uo_out = p;
+
+
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 
 endmodule
+
+module adder(
+    input a,
+    input b,
+    input c,
+    output y,
+    output z
+    );
+    
+// Internal Signals
+    wire int_sig1;
+    wire int_sig2;
+    wire int_sig3;
+    
+        
+    xor(y,a,b,c);
+    and(int_sig1,a,b);
+    and(int_sig2,a,c);
+    and(int_sig3,b,c);
+    or(z,int_sig1,int_sig2,int_sig3);
+     
+endmodule
+
+module part(
+    input [3:0] m,
+    input [2:0] y,
+    input q4,
+    input c,
+    output [2:0] o,
+    output co,
+    output p);
+    wire [2:0] w;
+    
+    adder stage0 (m[0]&c,y[0],0,p,w[0]);
+    adder stage1 (m[1]&c,y[1],w[0],o[0],w[1]);
+    adder stage2 (m[2]&c,y[2],w[1],o[1],w[2]);
+    adder stage3 (m[3]&c,q4,w[2],o[2],co);
+  
+endmodule 
--- test/test.py
+++ test/test.py
@@ -14,27 +14,38 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
     dut._log.info("Test project behavior")
-
+    
+    #Test #1
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in.value = 0x42
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
+    assert dut.uo_out.value == 0x08
+    
+    #Test #2
+    dut.ui_in.value = 0x55
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 25
+    
+    #Test #3
+    dut.ui_in.value = 0x38
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 24
+    
+    #Test #4
+    dut.ui_in.value = 0x67
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 42
+    
+    #Test #5
+    dut.ui_in.value = 0x26
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 12
+    
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
