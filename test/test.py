ab967e7c3499d186a621171e8180c3b6f10cf1c7
Jack Li <jl15060@nyu.edu>
2024-10-25 16:29:56 -0400

    13th try

--- test/test.py
+++ test/test.py
@@ -14,38 +14,46 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
     dut._log.info("Test project behavior")
-    
-    #Test #1
+
     # Set the input values you want to test
-    dut.ui_in.value = 0x42
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 0x08
-    
-    #Test #2
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
+
+    #test1
+    dut.ui_in.value = 0x42
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 8
+
+    #test2
     dut.ui_in.value = 0x55
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 25
     
-    #Test #3
-    dut.ui_in.value = 0x38
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 24
-    
-    #Test #4
+    #test3
     dut.ui_in.value = 0x67
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 42
-    
-    #Test #5
+
+    #test4
     dut.ui_in.value = 0x26
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 12
-    
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+
+    #test5
+    dut.ui_in.value = 0x38
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 24
